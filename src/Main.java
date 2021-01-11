a02334392ac31e01eb2778d66b92bf426bc8a4e4
Laura <lhernan53112@gmail.com>
2021-01-10 19:53:46 -0500

    Add method add and updates

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,17 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+	
+	static ArrayList<Integer> expenses = new ArrayList<>();
 
     public static void main(String[] args) {
+    	 
+    	  expenses.add(1000);
+          expenses.add(2300);
+          expenses.add(45000);
+          expenses.add(32000);
+          expenses.add(110);
+          
         /*System.out.println("Hello World!");*/
         System.out.println("\n**************************************\n");
         System.out.println("\tWelcome to TheDesk \n");
@@ -12,32 +21,23 @@ public class Main {
 
     }
     private static void optionsSelection() {
-        String[] arr = {"1. I wish to review my expenditure",
+        String[] quesOptions = {
+        		"1. I wish to review my expenditure",
                 "2. I wish to add my expenditure",
                 "3. I wish to delete my expenditure",
                 "4. I wish to sort the expenditures",
                 "5. I wish to search for a particular expenditure",
                 "6. Close the application"
         };
-        int[] arr1 = {1,2,3,4,5,6};
-        int  slen = arr1.length;
-        for(int i=0; i<slen;i++){
-            System.out.println(arr[i]);
-            // display the all the Strings mentioned in the String array
+
+      // display the all the Strings mentioned in the String array
+        for( String option: quesOptions) {
+        	 System.out.println(option);
         }
-        ArrayList<Integer> arrlist = new ArrayList<Integer>();
-        ArrayList<Integer> expenses = new ArrayList<Integer>();
-        expenses.add(1000);
-        expenses.add(2300);
-        expenses.add(45000);
-        expenses.add(32000);
-        expenses.add(110);
-        expenses.addAll(arrlist);
+
         System.out.println("\nEnter your choice:\t");
         Scanner sc = new Scanner(System.in);
         int  options =  sc.nextInt();
-        for(int j=1;j<=slen;j++){
-            if(options==j){
                 switch (options){
                     case 1:
                         System.out.println("Your saved expenses are listed below: \n");
@@ -49,7 +49,6 @@ public class Main {
                         int value = sc.nextInt();
                         expenses.add(value);
                         System.out.println("Your value is updated\n");
-                        expenses.addAll(arrlist);
                         System.out.println(expenses+"\n");
                         optionsSelection();
 
@@ -81,9 +80,7 @@ public class Main {
                         System.out.println("You have made an invalid choice!");
                         break;
                 }
-            }
-        }
-
+    
     }
     private static void closeApp() {
         System.out.println("Closing your application... \nThank you!");
@@ -92,6 +89,7 @@ public class Main {
         int leng = arrayList.size();
         System.out.println("Enter the expense you need to search:\t");
         //Complete the method
+        
     }
     private static void sortExpenses(ArrayList<Integer> arrayList) {
         int arrlength =  arrayList.size();
